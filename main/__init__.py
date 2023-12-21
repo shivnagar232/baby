ad72912930af449f2eec90483232eae16a219c97
shivnagar232 <154045191+shivnagar232@users.noreply.github.com>
2023-12-21 19:32:53 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -15,7 +15,7 @@ logging.getLogger("telethon").setLevel(logging.WARNING)
 # variables
 API_ID = "26075120" #config("API_ID", default=None, cast=int)
 API_HASH = "1fda88a5d1de46058a4791c78bce198e" #config("API_HASH", default=None)
-BOT_TOKEN = "6468925244:AAFi5pbHG0hRSQYF7sI6BaYWjUZciOarpxo" #config("BOT_TOKEN", default=None)
+BOT_TOKEN = "6508501033:AAHIFTa3Xbkx5NjcQPBnukt_KxdmSXfwHs4" #config("BOT_TOKEN", default=None)
 SESSION = "BQGTAMIAYFP0yfcy0sII4sAzyK5JjbGgFiMb6gVHy44_z4mACuzU6Gl8MGMl5h_89_Rt5kwcCskrNkRlQKA6NeS1R7FNzLy21QSKNxqTblzdwdyQGVNId15YnhH4ZZAGZiiJ69M0h6SRwac_b3m9nOHQnReXi0wdJ8tEG9CYc7kHXMLzQD8DrJh_RmJOnMwl0fSe_8FAxB4abVImyAHOOyZoqjrzeuPD-sSkUotycQ2IqXie6tyiNBypAmSfniAIkVGZXj500fJvb30X7_04C3IMYijpCzx22Mf_npl-xLavYBgOszFRT8SeQ6o6mBcR3svC-u3MMIvxE2JOmq59IskX9ZBDIgAAAAAiRSpcAA" #config("SESSION", default=None)
 FORCESUB = "dev_gagan" #config("FORCESUB", default=None)
 AUTH = "574958172" #config("AUTH", default=None)
